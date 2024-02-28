9a1b25f9d066912e079bd283e727f5fb28630947
thanoskour <cs04392@uoi.gr>
2024-02-29 01:15:12 +0200

    React-Node-App

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+
+
+app.use(cors());
+
+app.get('/houses', async (req, res) => {
+  try {
+    const response = await axios.get('https://wizard-world-api.herokuapp.com/houses');
+    const houses = response.data; // The data is available directly as response.data
+
+    // Apply your filtering logic here
+    const { name } = req.query;
+    const filteredHouses = name ? houses.filter(house => house.name.includes(name)) : houses;
+
+    res.json(filteredHouses); // Send the filtered data back as JSON
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
